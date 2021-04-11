c856a66f8480751dbc0866b7b12602b2a4e8ee41
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-11 06:15:59 -0600

    Update movielist.js

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -37,15 +37,15 @@ class MovieList extends Component {
             return (
                 <Carousel onSelect={this.handleSelect}>
                     {movieList.map((movie) =>
-                        <Carousel.Item key={movie._id}>
+                        <Carousel.Item key={movie.Title}>
                             <div>
-                                <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
+                                <LinkContainer to={'/movie/'+movie.Title} onClick={()=>this.handleClick(movie)}>
                                     <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
                                 </LinkContainer>
                             </div>
                             <Carousel.Caption>
-                                <h3>{movie.title}</h3>
-                                <BsStarFill glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+                                <h3>{movie.Title}</h3>
+                                <BsStarFill glyph={'star'} /> {movie.Rating} &nbsp;&nbsp; {movie.Year}
                             </Carousel.Caption>
                         </Carousel.Item>
                     )}
@@ -67,4 +67,5 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MovieList);
+
 
